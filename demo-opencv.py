7f11214affac59d4aacbbc160c2635e285dd29f3
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-09-06 22:13:48 +0700

    create keyboard

--- demo-opencv.py
+++ demo-opencv.py
@@ -6,23 +6,32 @@ cap.set(3, 1920)
 cap.set(4, 1080)
 detector = HandDetector(detectionCon=0.8)
 
-keys = [["Q", "W", "E", "R", "T", "Y", "U", "I"]]
+keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
+        ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
+        ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
 
 lmList = 0
 bbox = 0
+
+def drawAll(img, buttonList):
+  for button in buttonList:
+    x, y = button.pos
+    w, h = button.size
+    cv2.rectangle(img, button.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
+    cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+    return img
 
 class Button():
   def __init__(self, pos, text, size=[85, 85]):
     self.pos = pos
     self.size = size
     self.text = text
-    x, y = self.pos
-    w, h = self.size
-    cv2.rectangle(img, self.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
-    cv2.putText(img, self.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
 
+buttonList = []
 
-buttonLst = []
+for i in range(len(keys)):
+  for j, key in enumerate(keys[i]):
+    buttonList.append(Button([100 * j + 50, 100 * i + 50], key))
 
 while True:
     success, img = cap.read()
@@ -48,8 +57,7 @@ while True:
         bbox = bbox2
 
 
-    for x in range (0, 8):
-      buttonLst.append(Button([100 * x + 50, 100], "Q"))
+    img = drawAll(img, buttonList)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
