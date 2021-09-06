fa8e541385a71f5411c6930d5f3c123049f06948
Nguyen Tri Nhan <nguyentrinhan.dev@gmail.com>
2021-09-06 22:45:56 +0700

    write text

--- demo-opencv.py
+++ demo-opencv.py
@@ -10,8 +10,10 @@ keys = [["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
         ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
         ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "/"]]
 
-lmList = 0
-bbox = 0
+lmList = []
+bbox = []
+
+finalText = ""
 
 def drawAll(img, buttonList):
   for button in buttonList:
@@ -19,7 +21,7 @@ def drawAll(img, buttonList):
     w, h = button.size
     cv2.rectangle(img, button.pos, (x+w, y+h), (255, 0, 255), cv2.FILLED)
     cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
-    return img
+  return img
 
 class Button():
   def __init__(self, pos, text, size=[85, 85]):
@@ -44,8 +46,8 @@ while True:
       bbox1 = hand1["bbox"]
       centerPoint1 = hand1["center"]
       handType1 = hand1["type"]
-      lmList = lmList1
-      bbox = bbox1
+      lmList.append(lmList1)
+      bbox.append(bbox1)
 
       if len(hands) == 2:
         hand2 = hands[1]
@@ -53,11 +55,28 @@ while True:
         bbox2 = hand2["bbox"]
         centerPoint2 = hand2["center"]
         handType2 = hand2["type"]
-        lmList = lmList2
-        bbox = bbox2
-
+        lmList.append(lmList2)
+        bbox.append(bbox2)
 
     img = drawAll(img, buttonList)
+
+    if lmList:
+      for button in buttonList:
+        x, y = button.pos
+        w, h = button.size
+        if x < lmList1[8][0] < x + w and y < lmList1[8][1] < y + h:
+          cv2.rectangle(img, button.pos, (x+w, y+h), (175, 0, 175), cv2.FILLED)
+          cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+          l, _ = detector.findDistance(lmList1[8], lmList1[12])
+          ## When click
+          if l < 35:
+            cv2.rectangle(img, button.pos, (x+w, y+h), (0, 255, 0), cv2.FILLED)
+            cv2.putText(img, button.text, (x + 20, y + 65), cv2.FONT_HERSHEY_PLAIN, 4, (255, 255, 255), 4)
+            finalText += button.text
+
+
+    cv2.rectangle(img, (50, 350), (700, 450), (175, 0, 175), cv2.FILLED)
+    cv2.putText(img, finalText, (60, 500), cv2.FONT_HERSHEY_PLAIN, 5, (255, 255, 255), 5)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
